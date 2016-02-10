523caf124b622dbd96cce65f5984009ff8999853
Ryan Weaver <ryan@thatsquality.com>
2016-02-10 07:29:26 -0500

    Model: Add Product model

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Model\Product;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -16,8 +17,20 @@ class ProductController extends Controller
         $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         $result = $db->query('SELECT * FROM product');
 
+        $products = [];
+        foreach ($result as $row) {
+            $product = new Product();
+            $product->setId($row['id']);
+            $product->setName($row['name']);
+            $product->setDescription($row['description']);
+            $product->setPrice($row['price']);
+            $product->setCreatedAt(\DateTime::createFromFormat('Y-m-d H:i:s', $row['created_at']));
+
+            $products[] = $product;
+        }
+
         return $this->render('product/index.html.twig', [
-            'rows' => $result
+            'products' => $products
         ]);
     }
 }
---
+++ src/AppBundle/Model/Product.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Model;
+
+class Product
+{
+    private $id;
+
+    private $name;
+
+    private $description;
+
+    private $price;
+
+    private $createdAt;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+}
