fcd4ba107b796a6bdb71c1b4a2b2f3c32e3fedc6
Ryan Weaver <ryan@thatsquality.com>
2016-02-10 07:29:33 -0500

    Security: Empty User class

---
+++ src/AppBundle/Model/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Model;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class User implements UserInterface
+{
+    public function getUsername()
+    {
+        // TODO: Implement getUsername() method.
+    }
+
+    public function getRoles()
+    {
+        // TODO: Implement getUsername() method.
+    }
+
+    public function getPassword()
+    {
+        // TODO: Implement getPassword() method.
+    }
+
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+}
