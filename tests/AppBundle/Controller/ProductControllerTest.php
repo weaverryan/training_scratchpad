c82dc94e32c5dd37069cf14c5be7dd0d11a20860
Ryan Weaver <ryan@thatsquality.com>
2016-02-10 07:29:31 -0500

    Functional: Test form

--- tests/AppBundle/Controller/ProductControllerTest.php
+++ tests/AppBundle/Controller/ProductControllerTest.php
@@ -19,4 +19,21 @@ class ProductControllerTest extends \PHPUnit_Framework_TestCase
         $page = $session->getPage();
         $this->assertCount(2, $page->findAll('css', 'h3'));
     }
+
+    public function testAddProducts()
+    {
+        $driver = new GoutteDriver();
+        $session = new Session($driver);
+
+        $session->visit('http://localhost:9004/app_test.php/products');
+
+        $page = $session->getPage();
+        $page->clickLink('New Product');
+        $page->fillField('Name', 'Foo');
+        $page->fillField('Description', 'Description');
+        $page->fillField('MSRP', '5.99');
+        $page->pressButton('Save');
+
+        $this->assertContains('You did it!', $page->getText());
+    }
 }
