41291998fa8aa59f58a4a1557589b8d608bffc73
Ryan Weaver <ryan@thatsquality.com>
2016-02-10 07:29:34 -0500

    CompilerPass: Create empty pass

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -2,8 +2,15 @@
 
 namespace AppBundle;
 
+use AppBundle\DependencyInjection\OverrideLoggerCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AppBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new OverrideLoggerCompilerPass());
+    }
+
 }
---
+++ src/AppBundle/DependencyInjection/OverrideLoggerCompilerPass.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class OverrideLoggerCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        $loggerDefinition = $container->findDefinition('logger');die;
+
+    }
+}
