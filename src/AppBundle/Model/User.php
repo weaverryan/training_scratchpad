53d4b1c8f5577ef29e6880b519b90ee0278e63fa
Ryan Weaver <ryan@thatsquality.com>
2016-02-10 07:29:33 -0500

    Security: Finish User class

--- src/AppBundle/Model/User.php
+++ src/AppBundle/Model/User.php
@@ -6,28 +6,32 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class User implements UserInterface
 {
+    private $username;
+
+    public function __construct($username)
+    {
+        $this->username = $username;
+    }
+
     public function getUsername()
     {
-        // TODO: Implement getUsername() method.
+        return $this->username;
     }
 
     public function getRoles()
     {
-        // TODO: Implement getUsername() method.
+        return ['ROLE_USER'];
     }
 
     public function getPassword()
     {
-        // TODO: Implement getPassword() method.
     }
 
     public function getSalt()
     {
-        // TODO: Implement getSalt() method.
     }
 
     public function eraseCredentials()
     {
-        // TODO: Implement eraseCredentials() method.
     }
 }
